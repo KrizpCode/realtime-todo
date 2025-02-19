c5899ac553e8c31ac2436603bd0ff0bc8b459154
KrizpCode <93johan.e@gmail.com>
2025-02-19 23:04:20 +0100

    Fixed basic setup of tanstack router

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['routeTree.gen.ts'] },
   {
     extends: [
       js.configs.recommended,
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Hello World! 👋</h1>
+}
+
+export default Home
