3e1d221770c93bcb5009b52982716f684c76cbca
KrizpCode <93johan.e@gmail.com>
2025-02-20 23:11:31 +0100

    Setup of proxying request to API from client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,33 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    react()
-  ],
-  server: {
-    port: 3000
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      tailwindcss(),
+      TanStackRouterVite({ autoCodeSplitting: true }),
+      react()
+    ],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/tests/setup.ts'
+    }
   }
 })
