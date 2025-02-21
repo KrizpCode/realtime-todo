79017537f337fc3ad75df2a54c8581b87c65681b
KrizpCode <93johan.e@gmail.com>
2025-02-21 22:24:05 +0100

    Added a logout button to the header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,19 @@
-import { Link } from '@tanstack/react-router'
+import { Link, useNavigate, useRouter } from '@tanstack/react-router'
+import { useAuth } from '../hooks/useAuth'
 
 const Header = () => {
+  const { user, logout } = useAuth()
+  const router = useRouter()
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    await logout()
+
+    await router.invalidate()
+
+    await navigate({ to: '/login' })
+  }
+
   return (
     <header className="fixed z-40 flex h-12 w-full justify-between border-b-1 border-[#eef0f1] bg-[#f4f5f6]">
       <div className="flex items-center gap-3 pl-4">
@@ -10,18 +23,29 @@ const Header = () => {
         <h1 className="text-xl font-semibold">Todo</h1>
       </div>
       <div className="center flex items-center gap-2 pr-4">
-        <Link
-          to="/register"
-          className="flex min-h-7 cursor-pointer items-center rounded-sm bg-[#006fff] px-4 text-sm text-white hover:bg-[#338BFF] active:bg-[#0058CC]"
-        >
-          Register
-        </Link>
-        <Link
-          to="/login"
-          className="flex min-h-7 cursor-pointer items-center rounded-sm px-4 text-sm text-[#676f79] hover:bg-[#2123270A]"
-        >
-          Login
-        </Link>
+        {user ? (
+          <button
+            onClick={handleLogout}
+            className="flex min-h-7 cursor-pointer items-center rounded-sm px-4 text-sm text-[#676f79] hover:bg-[#2123270A]"
+          >
+            Logout
+          </button>
+        ) : (
+          <>
+            <Link
+              to="/register"
+              className="flex min-h-7 cursor-pointer items-center rounded-sm bg-[#006fff] px-4 text-sm text-white hover:bg-[#338BFF] active:bg-[#0058CC]"
+            >
+              Register
+            </Link>
+            <Link
+              to="/login"
+              className="flex min-h-7 cursor-pointer items-center rounded-sm px-4 text-sm text-[#676f79] hover:bg-[#2123270A]"
+            >
+              Login
+            </Link>
+          </>
+        )}
       </div>
     </header>
   )
