a47c8a7f04852d46b22bad0f5b75f45a2411868f
KrizpCode <93johan.e@gmail.com>
2025-02-24 01:37:43 +0100

    Skip preload of todo-lists

--- client/src/routes/_auth/todo-lists/$todoListUUID.tsx
+++ client/src/routes/_auth/todo-lists/$todoListUUID.tsx
@@ -14,5 +14,6 @@ export const Route = createFileRoute('/_auth/todo-lists/$todoListUUID')({
     opts.context.queryClient.ensureQueryData(
       todoListQueryOptions(opts.params.todoListUUID)
     ),
+  preload: false,
   component: TodoListPage
 })
